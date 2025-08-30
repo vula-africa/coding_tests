99bb2b269b729747057a6ca34a85e992069d1850
Kimani Ndegwa <kimanindegwa@Kimanis-MacBook-Pro.local>
2025-08-30 22:37:05 +0300

    chore: map out pseudocode for the exisint function as is

--- tests/unsubmitted_forms/cleanup_unsubmitted_forms.ts
+++ tests/unsubmitted_forms/cleanup_unsubmitted_forms.ts
@@ -21,10 +21,54 @@
  * Expected time: 45-60 minutes
  */
 
+
+/**
+ * Pseudo Code Implemenations:
+ * 
+ */
+
 // For the purpose of this test you can ignore that the imports are not working.
 import type { JobScheduleQueue } from "@prisma/client";
 import { prisma } from "../endpoints/middleware/prisma";
 import { update_job_status } from "./generic_scheduler";
+
+
+/**
+ * 
+ * currentPseudocode:
+ * function cleanup_unsubmitted_forms(job):
+ *    - TRY
+ *      - SET sevendaysAgo to new Date(Date.now() - 7 * 24 * 60 * 60); 
+ *      - SET sevendaysAgoPlusOneDay to new Date(sevendaysAgo.getTime() + 24 * 60 * 60 * 1000);
+ *      - SET expiredTokens to await prisma.publicFormsTokens.findMany({
+ *        where: {
+ *          createdAt: {
+ *            gte: sevendaysAgo,  
+ *            lt: sevendaysAgoPlusOneDay,
+ *          },
+ *        },
+ *      });
+ *      - FOR each expiredToken in expiredTokens
+ *        - SET relationship to await prisma.relationship.findFirst({
+ *          where: {
+ *            product_id: expiredToken.productId,
+ *            status: "new",
+ *          },
+ *        });
+ *        - IF relationship exists
+ *          - await prisma.$transaction([
+ *            - DELETE relationship
+ *            - DELETE the token
+ *            - DELETE all corpus items associated with the entity
+ *            - DELETE the entity (company)
+ *      - ELSE
+ *        - DO NOTHING
+ *    - CATCH error
+ *      - LOG error
+ * 
+ */
+
+
 
 export const cleanup_unsubmitted_forms = async (job: JobScheduleQueue) => {
   try {
@@ -34,6 +78,7 @@ export const cleanup_unsubmitted_forms = async (job: JobScheduleQueue) => {
       sevenDaysAgo.getTime() + 24 * 60 * 60 * 1000
     );
 
+    
     const expiredTokens = await prisma.publicFormsTokens.findMany({
       where: {
         createdAt: {
